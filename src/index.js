8ff805ab4b28befaea6ffed4051965e95fc301a3
Yathaartha <--replace-all>
2021-05-08 20:46:29 +0545

    reused components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
-// import the react and reactDOM elements
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
-// Create a react component
+const comments = [
+  {
+    author: "Sam",
+    date: "Today at 6:00PM",
+    message: "Nice blog post!!",
+    avatar: faker.image.avatar(),
+  },
+  {
+    author: "Alex",
+    date: "Yesterday at 8:00PM",
+    message: "Good blog post!",
+    avatar: faker.image.avatar(),
+  },
+  {
+    author: "Jane",
+    date: "Yesterday at 7:00AM",
+    message: "Wew blog post!",
+    avatar: faker.image.avatar(),
+  },
+];
+
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div className="ui container comments">
+      {comments.map((comment, index) => {
+        return (
+          <ApprovalCard>
+            <CommentDetail {...comment} key={index} />
+          </ApprovalCard>
+        );
+      })}
+    </div>
+  );
 };
 
-// take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+export default App;
